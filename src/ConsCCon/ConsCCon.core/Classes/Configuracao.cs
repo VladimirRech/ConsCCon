2352b3b253959812d5dfb8c15ae882b55947c186
Vladimir Rech <rechvladimir@hotmail.com>
2024-03-19 20:51:01 -0300

    Classe de configurações.

---
+++ src/ConsCCon/ConsCCon.core/Classes/Configuracao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsCCon.core
+{
+    public class Configuracao : BaseLog
+    {
+        public string CNPJCliente { get; set; }
+        public string UFCliente { get; set; }
+        public string PastaEnvioUninfe { get; set; }
+        public string PastaRetornoUninfe { get; set; }
+        public string PastaArquivoCSV { get; set; }
+        public int LinhaInicialBaseCNPJ { get; set; }
+        public int ColunaInicialBaseCnpj { get; set; }
+
+        public static Configuracao LeConfiguracoes()
+        {
+            return null;
+        }
+
+        public bool ValidaConfiguracao()
+        {
+            return false;
+        }
+    }
+}
