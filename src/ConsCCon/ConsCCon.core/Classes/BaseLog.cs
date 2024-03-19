7cd70263d71078327b750442ab968e6058c3ed32
Vladimir Rech <rechvladimir@hotmail.com>
2024-03-19 20:50:34 -0300

    Classe base de regras de negócio

---
+++ src/ConsCCon/ConsCCon.core/Classes/BaseLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsCCon.core
+{
+    /// <summary>
+    /// Estrutura base para as classes
+    /// </summary>
+    public class BaseLog
+    {
+        public string UltimaMsgErro { get; set; }
+        public DateTime UltimoTimeStampErro { get; set; }
+        public string StackErro { get; set; }
+
+        public void CapturaErro(Exception ex)
+        {
+            UltimaMsgErro = ex.Message;
+            UltimoTimeStampErro = DateTime.Now;
+            StackErro = ex.StackTrace;
+        }
+    }
+}
