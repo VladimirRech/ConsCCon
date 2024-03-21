94898b4b42926b6ce1a23c6c296bf254a81db03c
Vladimir Rech <rechvladimir@hotmail.com>
2024-03-21 20:25:50 -0300

    Novo exemplo de base CNPJ

--- src/Python/gerar_cnpj.py
+++ src/Python/gerar_cnpj.py
@@ -7,9 +7,15 @@ def generate_cnpj():
 def generate_company_name():
   words = ['Empresa', 'Fictícia', 'Comercial', 'Industrial', 'Ltda', 'SA']
   return ' '.join(random.choice(words) for _ in range(3))
+  
+def generate_company_state():
+  words = ['PR', 'SP', 'SC', 'RS', 'MG', 'RJ', 'BA']
+  return ' '.join(random.choice(words) for _ in range(1))
+  
 
-with open('new_file.csv', 'w') as f:
+with open('cnpjs.csv', 'w') as f:
   for _ in range(1000):
     cnpj = generate_cnpj()
     company_name = generate_company_name()
-    f.write(f'{cnpj};{company_name}\n')
+    state = generate_company_state()
+    f.write(f'{cnpj};{company_name};{state}\n')
